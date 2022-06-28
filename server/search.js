710db9f1a315a7f4e6b3991e44537a871bc7499f
HakeemDAbdulmalik <hakeem.d.abdulmalik@gmail.com>
2022-06-28 15:15:09 -0400

    Updated syntax to match airbnb requirements

--- server/search.js
+++ server/search.js
@@ -1,47 +1,35 @@
 const axios = require('axios');
 
-const url = 'http://gutendex.com/books'
+const url = 'http://gutendex.com/books';
 
 function search(querys) {
   let addQuery = '';
   if (querys) {
-    let queryKeys = Object.keys(querys);
+    const queryKeys = Object.keys(querys);
 
-    queryKeys.forEach(element => {
-      addQuery += `${element}=${querys[element]}&`
+    queryKeys.forEach((element) => {
+      addQuery += `${element}=${querys[element]}&`;
     });
   }
   return axios.get(`${url}?${addQuery}`)
-  .catch(err => (console.log('Error when calling axios. Function name: search, File: search.js, Folder: server. The error returned: ', err)));
+    .catch((err) => (console.log('Error when calling axios. Function name: search, File: search.js, Folder: server. The error returned: ', err)));
 }
 
 function getTxt(txtUrl = 'https://www.gutenberg.org/files/11/11-0.zip') {
-  let checkPostFix = txtUrl.substr((txtUrl.length - 3));
-
-
-
+  const checkPostFix = txtUrl.substr((txtUrl.length - 3));
+  let newUrl;
   if (checkPostFix === 'zip') {
-    txtUrl = txtUrl.substr(0, (txtUrl.length - 3));
-    txtUrl += 'txt';
+    newUrl = txtUrl.substr(0, (txtUrl.length - 3));
+    newUrl += 'txt';
   } else {
-    console.log('Check postfix : ', checkPostFix);
+    newUrl = txtUrl;
   }
 
-  return axios.get(txtUrl)
-  .catch(err => (console.log('Error when calling axios. Function name: search, File: search.js, Folder: server. The error returned: ', err)));
+  return axios.get(newUrl)
+    .catch((err) => (console.log('Error when calling axios. Function name: search, File: search.js, Folder: server. The error returned: ', err)));
 }
 
 module.exports = {
   search,
-  getTxt
-}
-
-// Start with creating a copy for each of the routes already created in gutendex
-// Check what data is actually desired: Just the books, language, and ????
-// Check what data is going to be saved in the database?
-// Create routes for sending what books a person is currently reading
-// Check out what is expected.
-
-// When query's come in the default value should be blank
-// Use map to go through each of the values.
-// Test with node first
+  getTxt,
+};
--- server/server.js
+++ server/server.js
@@ -1,47 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const cookieSession = require('cookie-session');
-const app = express();
-const search = require('./search.js');
+const search = require('./search');
+
 require('dotenv').config();
 
+const app = express();
+
 const corsOptions = {
-  origin: 'http://localhost:8081'
+  origin: 'http://localhost:8081',
 };
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     name: 'lit-hub-session',
     secret: process.env.COOKIE_SECRET,
-    httpOnly: true
-  })
+    httpOnly: true,
+  }),
 );
 
 app.get('/test', (req, res) => {
   res.json({ message: 'Welcome to LitHub!', query: req.query });
-})
+});
 
 app.get('/search', (req, res) => {
-
   search.search(req.query)
-  .then(data => {
-    console.log(data.data);
-    res.json(data.data);
-  })
-  .catch(err => (console.log('/search is currently failing. Error: ', err)));
-})
+    .then((data) => {
+      console.log(data.data);
+      res.json(data.data);
+    })
+    .catch((err) => (console.log('/search is currently failing. Error: ', err)));
+});
 
 app.get('/txt', (req, res) => {
-
   search.getTxt(req.query.url)
-  .then(data => {
-    res.json(data.data);
-  })
-  .catch(err => (console.log('/txt is currently failing. Error: ', err)));
-})
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch((err) => (console.log('/txt is currently failing. Error: ', err)));
+});
 
 app.listen(process.env.PORT, () => {
-  console.log('Server listening on port ' + process.env.PORT);
-})
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
