fb23d50ff97ca097b710615b379376758c857e79
Justin C Kirk <justin.c.kirk@gmail.com>
2022-06-29 16:25:55 -0400

    txt route now returns string instead of object

--- server/search.js
+++ server/search.js
@@ -6,7 +6,6 @@ function search(clientQuery) {
   let addQuery = '';
   if (clientQuery) {
     const queryKeys = Object.keys(clientQuery);
-
     queryKeys.forEach((element) => {
       addQuery += `${element}=${clientQuery[element]}&`;
     });
@@ -16,17 +15,10 @@ function search(clientQuery) {
 }
 
 function getTxt(txtUrl) {
-  console.log(txtUrl);
   return axios.get(txtUrl)
     .then((data) => {
       console.log(typeof data.data);
-      console.log(data.data.indexOf('<body>'), data.data.indexOf('</body>'));
-      let parsedData = data.data.slice(data.data.indexOf('<body>'), data.data.indexOf('</body>') + 7);
-
-      // console.log(parsedData);
-      // const dataResults = { data: data.data.slice(data.data.indexOf('<body>'), (data.data.indexOf('</body>') + 7)) };
-      // const dataString = `${data.data}`;
-      // console.log(data);
+      const parsedData = data.data.slice(data.data.indexOf('<body>'), data.data.indexOf('</body>') + 7);
       return parsedData;
     })
     .catch((err) => (console.log('Error when calling axios. Function name: search, File: search.js, Folder: server. The error returned: ', err)));
