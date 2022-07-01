13c1756e360349bc4105f239777546e94fd69140
arkteck <arkteck@gmail.com>
2022-07-01 11:57:59 -0400

    Disable etag

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.disable('etag');
 
 app.use((req, res, next) => {
   console.log(req.method, req.url);
